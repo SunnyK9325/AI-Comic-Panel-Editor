9382f4974178fd71fa2c6034e8c07029853a9ded
Sunny <sunnykummar9325@gmail.com>
2023-11-17 21:57:59 +0530

    Intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,227 @@
-import logo from './logo.svg';
 import './App.css';
+import './components/Pagination.css'
+import './components/PaginationItem.css'
+import { useEffect, useState } from "react";
+import Pagination from './components/Pagination'
+import { query } from './api/handle';
+import { GoCommandPalette } from "react-icons/go";
+import { HiOutlineAnnotation } from "react-icons/hi";
+import { CiGrid2H } from "react-icons/ci";
 
 function App() {
+
+  const [inputText, setInputText] = useState('');
+  const [imageArray, setImageArray] = useState(Array.from({ length: 10 }));
+  const [submitBtn, setSubmitBtn] = useState(false)
+  // for Pagination
+  const [currentPage, setCurrentPage] = useState(1)
+  const [isTextEnabled, setIsTextEnabled] = useState(false);
+  const [textValue, setTextValue] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [bubbleArray, setBubbleArray] = useState(Array.from({ length: 10 }));
+
+  const [showScrollButton, setShowScrollButton] = useState(false);
+
+  const handleQuery = async () => {
+    setIsLoading(true); // Set isLoading to true while fetching
+    try {
+      // Check if the textarea is empty 
+      if (inputText === '') {
+        alert('Please enter some input. ')
+        return;
+      }
+
+      // First as soon as someone submits the prompt 
+      // the previous image for that page should be removed. 
+      setImageArray((prevImages) => {
+        const newImages = [...prevImages];
+        newImages[currentPage - 1] = undefined;
+        return newImages;
+      });
+
+      const response = await query({ "inputs": inputText });
+      const imageUrl = URL.createObjectURL(response);
+
+      setImageArray((prevImages) => {
+        const newImages = [...prevImages];
+        newImages[currentPage - 1] = imageUrl;
+        return newImages;
+      });
+      setInputText('');
+
+    } catch (error) {
+      console.log('Fetching image failed:', error);
+      setIsLoading(false); // Set isLoading to false when the image is loaded or in case of an error
+    } finally {
+      setIsLoading(false); // Set isLoading to false when the image is loaded or in case of an error
+    }
+  };
+
+
+  const handleText = () => {
+    // This sets the array value as per the text for 
+    // the corresponding page
+    setBubbleArray((prevText) => {
+      const newText = [...prevText];
+      newText[currentPage - 1] = textValue;
+      return newText;
+    });
+
+    setTextValue('');
+  }
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // Show the scroll-to-top button when scrolling down, hide it when at the top
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+      setShowScrollButton(scrollY > 100);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const handleScrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  const filteredImages = imageArray.filter((image) => image !== undefined);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='lg:mx-9 min-h-screen mx-auto flex flex-wrap md:flex-nowrap 
+        items-center  justify-center py-3'>
+        {/* Input Panel  */}
+
+        <div className='panel'>
+          <span style={{ color: 'black', fontSize: '35px', marginBottom: '15px' }}>
+            Panel Editor
+          </span>
+
+          <hr style={{ marginBottom: '25px', borderBottom: '2px solid red', width:'75%' }} />
+
+          {/* prompt box */}
+          <div className="promptBox">
+          <span style={{ color: 'grey', fontSize:'20px' }}>Prompt</span>
+            <textarea
+              required={true}
+              value={inputText}
+              placeholder='Enter prompt to generate an image'
+              className='textarea'
+              onChange={(e) => setInputText(e.target.value)}
+            />
+
+            {/* Prompt generate Button */}
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <button onClick={handleQuery} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <GoCommandPalette style={{ marginRight: '6px', fontWeight: 'bold', fontSize: '16px' }} /> Generate
+              </button>
+              <button onClick={() => setSubmitBtn(!submitBtn)} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                {!submitBtn && <CiGrid2H style={{ marginRight: '6px', fontWeight: 'bold', fontSize: '16px' }} />}
+                {
+                  submitBtn ? 'Hide Preview' : 'See Preview'
+                }
+              </button>
+            </div>
+          </div>
+
+          {/* Add space between prompt box and speech bubble box */}
+          <div style={{ margin: '1rem 0' }}></div>
+
+          {/* speech bubble box */}
+          <div className='textBox'>
+            <div>
+              <label className="flex items-center space-x-2">
+                <input
+                  type="checkbox"
+                  checked={isTextEnabled}
+                  onChange={() => setIsTextEnabled(!isTextEnabled)}
+                  className="form-checkbox h-5 w-5 text-blue-600"
+                />
+                <span style={{ color: 'grey', fontSize:'18px' }}>Customise Speech Bubble</span>
+              </label>
+            </div>
+
+
+            <textarea
+              value={textValue}
+              placeholder='Enter text'
+              onChange={(e) => setTextValue(e.target.value)}
+              style={!isTextEnabled ? { opacity: '0.5', cursor: 'not-allowed' } : {}}
+              disabled={!isTextEnabled}
+              className='textarea'
+            />
+
+            {/* Speech Bubble Button */}
+            <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+              <button onClick={handleText} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <HiOutlineAnnotation style={{ marginRight: '6px', fontWeight: 'bold', fontSize: '16px' }} />Add Text </button>
+            </div>
+
+          </div>
+        </div>
+        {/* -------------------------------------------------------------------------------------------------- */}
+
+        {/* for Pagination */}
+        <div className='container'>
+        <div style={{display:'flex', alignItems:'center', justifyContent:'center'}}>
+        <span style={{fontSize:'20px'}}>Comic Panel Display</span>
+        </div>
+          <Pagination
+            currentPage={currentPage}
+            total={10}
+            limit={1}
+            onPageChange={(page) => setCurrentPage(page)}
+            imageArray={imageArray}
+            isLoading={isLoading}
+            bubbleArray={bubbleArray}
+          />
+        </div>
+      </div>
+
+      {/* preview comic section  */}
+      <div className={`mx-auto flex flex-col justify-center items-center ${submitBtn ? 'my-40' : ''}`}>
+        {
+          submitBtn ?
+            <>
+              <span></span>
+              <div className="relative">
+                {filteredImages.map((imageSrc, index) => (
+                  <div key={index} className="relative">
+                    <img
+                      src={imageSrc}
+                      alt={`Fetched ${index + 1}`}
+                      className='block border-[0.1rem] rounded my-2 max-h-[50rem] max-w-[50rem] md:w-[80%] md:h-[80%]'
+                    />
+                    {bubbleArray[index] && (
+                      <div key={index} className="speech top-right">
+                        {bubbleArray[index]}
+                      </div>
+                    )}
+                  </div>
+                ))}
+              </div>
+            </>
+            :
+            null
+
+        }
+      </div>
+
+      {/* scroll to top button */}
+      {showScrollButton && (
+        <button className="scroll-to-top" onClick={handleScrollToTop}>
+          &#9650;
+        </button>
+      )}
+    </>
+
   );
 }
-
 export default App;
